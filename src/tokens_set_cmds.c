070433b0cda6937208d7fd52bf9135587843bf7c
Eugene <eugenelct917@gmail.com>
2024-04-09 18:59:08 +0800

    norm all done except minishell.h

--- src/tokens_set_cmds.c
+++ src/tokens_set_cmds.c
@@ -6,7 +6,7 @@
 /*   By: cliew <cliew@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 17:13:35 by root              #+#    #+#             */
-/*   Updated: 2024/04/07 11:17:05 by cliew            ###   ########.fr       */
+/*   Updated: 2024/04/09 18:57:36 by cliew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,22 @@
 int	is_only_spaces(char *str)
 {
 	int	length;
+	int	i;
 
 	length = ft_strlen(str);
-    if ((str[0] == '\'' && str[length - 1] == '\'') || (str[0] == '"' && str[length - 1] == '"')) {
-        int i = 1;
-
-        while (i < length - 1) {
-            if (!isspace(str[i])) {
-                return 0; // Non-space character found
-            }
-            i++;
-        }
-
-        return 1;
-    }
-
-    return 0;
+	if ((str[0] == '\'' && str[length - 1] == '\'') || (str[0] == '"'
+			&& str[length - 1] == '"'))
+	{
+		i = 1;
+		while (i < length - 1)
+		{
+			if (!isspace(str[i]))
+			{
+				return (0);
+			}
+			i++;
+		}
+		return (1);
+	}
+	return (0);
 }
-
