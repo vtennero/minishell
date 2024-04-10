468dd1f7c0f22183fb1c40fbf603635942e8bb28
Victor Tenneroni <vitenner@c1r5s2.42singapore.sg>
2024-04-10 10:59:47 +0800

    fixed grep norm

--- src/tokens_set_cmds.c
+++ src/tokens_set_cmds.c
@@ -6,7 +6,7 @@
 /*   By: vitenner <vitenner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 17:13:35 by root              #+#    #+#             */
-/*   Updated: 2024/04/10 10:48:34 by vitenner         ###   ########.fr       */
+/*   Updated: 2024/04/10 10:59:07 by vitenner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	is_only_spaces(char *str)
 
 	length = ft_strlen(str);
 	if ((str[0] == '\'' && str[length - 1] == '\'') || (str[0] == '"' && str[length - 1] == '"')) {
-		int i = 1;
+		i = 1;
 
 		while (i < length - 1) {
 			if (!ft_isspace(str[i])) {
